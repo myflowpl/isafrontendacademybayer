ad92ee995246d7e8413f19a58edd02f8de9d436c
Kamil Richert <krichert@atlassian.com>
2022-12-16 14:14:44 +0100

    Added css modules

--- react/my-app/src/Contact.js
+++ react/my-app/src/Contact.js
@@ -1,7 +1,7 @@
-import './Contact.css';
+import styles from'./Contact.module.css';
 
 export function Contact({ data: { phone, address, email } }) {
-    return <div className="contact-container">
+    return <div className={styles.container}>
         <h4>PHONE</h4>
         <p>{phone}</p>
         <h4>ADDRESS</h4>
