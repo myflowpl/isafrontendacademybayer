27158326fc90e7e9c247df91c0d9f68dc354e2bb
Kamil Richert <krichert@atlassian.com>
2022-12-16 15:41:01 +0100

    Added MyFavouriteDish as list example

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,19 @@
 import MyName from './MyName';
 import { Contact } from './Contact';
 import { Wrapper } from './Wrapper';
+import { MyFavouriteDishes } from './MyFavouriteDishes';
 
 const data = {
     phone: '111 222 333',
     address: { street: 'Słowackiego', city: 'Gdańsk', number: 37 }, 
     email: 'moj@mail.com'
 }
+
+const dishes = [
+    { id: 1, name: 'Baklava' },
+    { id: 2, name: 'Gnocchi' },
+    { id: 3, name: 'Pizza' },
+]
 
 function App() {
 
@@ -15,6 +22,7 @@ function App() {
         <MyName name="Kamil" />
         <MyName name="Kamil" surname="Richert" />
         <Contact data={data} />
+        <MyFavouriteDishes dishes={dishes} />
     </Wrapper>
   );
 }
