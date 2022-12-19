888f63cd24fa663d9b2ae89130a33e745ca60e4d
Kamil Richert <krichert@atlassian.com>
2022-12-19 16:32:27 +0100

    Added Content component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,35 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
 import { Navigation } from './Navigation'
-import { Wrapper } from './Wrapper'
-import { AboutMe } from './about-me/AboutMe'
-import { Game } from './game/Game';
-import { GameClass } from './game/GameClass';
-import { Users } from './users/Users';
-import { MyForm } from './forms/MyForm';
-import { MyBetterForm } from './forms/MyBetterForm';
-import { MyBoostedForm } from './forms/MyBoostedForm';
+import { Content } from './Content'
+
 
 function App() {
 
   return (
     <>
         <Navigation />
-        <Wrapper>
-            <Routes>
-                <Route path="/" element={<h1>Welcome to our site!</h1>} />
-                <Route path="/about-me" element={<AboutMe />} />
-                <Route path="/forms" element={<>
-                        <MyBoostedForm />
-                        <MyBetterForm />
-                        <MyForm />
-                    </>} />
-                <Route path="/game" element={<>
-                        <GameClass name="Just Another Game" />
-                        <Game name="Catan" />
-                    </>} />
-                <Route path="/users" element={<Users />} />
-            </Routes>
-        </Wrapper>
+        <Content />
     </>
   );
 }
