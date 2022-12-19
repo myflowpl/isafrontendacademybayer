8ab5ea893ab0b27acb31827f1b40c825c6b07d67
Kamil Richert <krichert@atlassian.com>
2022-12-19 10:39:17 +0100

    Added showing fetched data in users

--- react/my-app/src/users/Users.js
+++ react/my-app/src/users/Users.js
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Users = () => {
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(r => r.json())
             .then(data => {
-                console.log(data)
+                setUsers(data)
             })
     }, []);
 
-    return <h1>Users</h1>
+    return <>
+        <h1>Users</h1>
+        <ul>
+            {users.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+    </>
 }
