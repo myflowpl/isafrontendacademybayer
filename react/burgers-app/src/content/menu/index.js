026094952f7dbe301af4af87bfe25bc335c87030
Kamil Richert <krichert@atlassian.com>
2022-12-20 15:02:43 +0100

    Added fetching data in menu with dispalying them in a table

--- react/burgers-app/src/content/menu/index.js
+++ react/burgers-app/src/content/menu/index.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react';
 import { PageWrapper } from "../../common/page-wrapper"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const Menu = () => {
+    const [burgers, setBurgers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rest-api-b6410.firebaseio.com/burgers.json')
+            .then(r => r.json())
+            .then(data => {
+                const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key] }));
+                setBurgers(formattedData);
+            })
+    }, []);
+
     return <PageWrapper title="Menu">
-    
+        <TableContainer component={Paper}>
+            <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Ingredients</TableCell>
+                    <TableCell align="right">Price ($)</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {burgers.map(burger => (
+                    <TableRow key={burger.id}>
+                        <TableCell>{burger.name}</TableCell>
+                        <TableCell align="right">{burger.ingredients}</TableCell>
+                        <TableCell align="right">{burger.price}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </TableContainer>
     </PageWrapper>
 }
