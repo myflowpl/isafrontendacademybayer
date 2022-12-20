d2057c0df99570f8b4ba90ba19a950d427d2f755
Kamil Richert <krichert@atlassian.com>
2022-12-20 11:07:07 +0100

    Init language context and use it in MyName component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,15 +1,23 @@
+import { createContext, useState } from 'react';
+import Button from '@mui/material/Button';
 import { Navigation } from './Navigation'
 import { Content } from './Content'
 
+export const LanguageContext = createContext('en');
 
 function App() {
+    const [lang, setLang] = useState('en');
 
-  return (
-    <>
-        <Navigation />
-        <Content />
-    </>
-  );
+    return (
+        <LanguageContext.Provider value={lang}>
+            <Navigation />
+            <div>
+                <Button onClick={() => setLang('en')}>EN</Button>
+                <Button onClick={() => setLang('pl')}>PL</Button>
+            </div>
+            <Content />
+        </LanguageContext.Provider>
+    );
 }
 
 export default App;
--- react/my-app/src/about-me/MyName.js
+++ react/my-app/src/about-me/MyName.js
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+import { LanguageContext } from '../App';
+
 function MyName(props) {
+    const language = useContext(LanguageContext);
+
     if (props.surname) {
+        if (language === 'pl') {
+            return <h1>Cześć moje imię to {props.name}, 
+               a nazwisko to {props.surname}!</h1>
+        }
+
         return <h1>Hello my name is {props.name}, 
             and my surname is {props.surname}!</h1>
+    }
+
+
+    if (language === 'pl') {
+        return <h1>Cześć moje imię to {props.name}!</h1>
     }
 
     return <h1>Hello my name is {props.name}!</h1>
