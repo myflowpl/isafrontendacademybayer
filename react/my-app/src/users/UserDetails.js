3fc4e75c933db4f09ad397ccd9afbbd49c9c3b57
Kamil Richert <krichert@atlassian.com>
2022-12-20 09:48:51 +0100

    Added loading and error state in user details

--- react/my-app/src/users/UserDetails.js
+++ react/my-app/src/users/UserDetails.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Typography from '@mui/material/Typography'
+import CircularProgess from '@mui/material/CircularProgress'
 
 export const UserDetails = () => {
     const params = useParams();
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
@@ -12,11 +14,18 @@ export const UserDetails = () => {
             .then(data => {
                 setUser(data)
             })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [params.id])
 
-    if (!user) {
-        return null;
+    if (isLoading) {
+        return <CircularProgess />
     }
 
-    return <Typography variant="h5">Hello {user.name} !</Typography>
+    if (!user || !user.name) {
+        return <Typography variant="h5">Such user does not exist!</Typography>;
+    }
+
+    return <Typography variant="h5">Hello {user.name}!</Typography>
 }
