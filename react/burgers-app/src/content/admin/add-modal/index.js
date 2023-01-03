9733c8be3cd74f4ccce67225af4137f1972f5d85
Kamil Richert <krichert@atlassian.com>
2023-01-03 10:59:09 +0100

    Added admin page and created button to interact with table

---
+++ react/burgers-app/src/content/admin/add-modal/index.js
@@ -0,0 +1,8 @@
+import Fab from '@mui/material/Fab';
+import Icon from '@mui/material/Icon';
+
+export const AddModalButton = () => {
+    return <Fab variant="contained" color="primary">
+        <Icon>add</Icon>
+    </Fab>
+}
--- react/burgers-app/src/content/admin/index.js
+++ react/burgers-app/src/content/admin/index.js
@@ -1,7 +1,67 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { PageWrapper } from "../../common/page-wrapper"
+import { AddModalButton } from './add-modal';
+import styled from 'styled-components';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+
+const ButtonContainer = styled.div`
+    margin-top: 20px;
+    align-self: flex-end;
+`;
 
 export const Admin = () => {
+    const [burgers, setBurgers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rest-api-b6410.firebaseio.com/burgers.json')
+            .then(r => r.json())
+            .then(data => {
+                const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key] }));
+                setBurgers(formattedData);
+            })
+    }, []);
+
     return <PageWrapper title="Admin">
-    
+        <TableContainer component={Paper}>
+            <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Ingredients</TableCell>
+                    <TableCell align="right">Price ($)</TableCell>
+                    <TableCell sx={{ width: '65px' }} />
+                    <TableCell sx={{ width: '85px' }} />
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {burgers.map(burger => (
+                    <TableRow key={burger.id}>
+                        <TableCell>
+                            <Link to={`/menu/${burger.id}`}>{burger.name}</Link>
+                        </TableCell>
+                        <TableCell align="right">{burger.ingredients}</TableCell>
+                        <TableCell align="right">{burger.price}</TableCell>
+                        <TableCell>
+                            <Button variant="contained" color="primary">Edit</Button>
+                        </TableCell>
+                        <TableCell>
+                            <Button variant="contained" color="error">Delete</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </TableContainer>
+        <ButtonContainer>
+            <AddModalButton />
+        </ButtonContainer>
     </PageWrapper>
 }
