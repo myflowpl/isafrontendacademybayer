daf642e579468a891c23fb3cf797757ac17db82d
Kamil Richert <krichert@atlassian.com>
2023-01-05 12:44:32 +0100

    Ex2 tests

--- tests/exercises/ex2/client.test.js
+++ tests/exercises/ex2/client.test.js
@@ -22,7 +22,25 @@ describe('getNextOrderNumber', () => {
   });
 
   test('should return correct n-th order number', () => {
+    //arrange
+    const service = new ClientService();
+    const client = {
+        orders: [1,2,3],
+      };
 
+    const expected = 'ISA-#4'
+
+    // act
+    const actual = service.getNextOrderNumber(client);
+
+    // assert
+    if (actual === expected) {
+        return;
+      } else {
+        throw new Error(
+          `Assertion failed! Actual="${actual}", where should be "${expected}"`
+        );
+      }
   });
 });
 
@@ -50,28 +68,139 @@ describe('calculateDiscount', () => {
   });
 
   test('should return no discount if orders value is less then 500', () => {
+    // arrange
+    const service = new ClientService();
+    const client = {
+      orders: [100, 399],
+    };
 
+    const expected = 0;
+
+    // act
+    const actual = service.calculateDiscount(client);
+
+    // assert
+    if (actual === expected) {
+        return;
+      } else {
+        throw new Error(
+          `Assertion failed! Actual="${actual}", where should be "${expected}"`
+        );
+      }
   });
 
   test('should 15% discount if client is internal', () => {
+   // arrange
+   const service = new ClientService();
+   const client = {
+     orders: [100, 399],
+     type: 'INTERNAL'
+   };
 
+   const expected = 0.15;
+
+   // act
+   const actual = service.calculateDiscount(client);
+
+   // assert
+   if (actual === expected) {
+       return;
+     } else {
+       throw new Error(
+         `Assertion failed! Actual="${actual}", where should be "${expected}"`
+       );
+     }
   });
 
   test('should return 25% discount if client is internal and spent more than 500', () => {
+   // arrange
+   const service = new ClientService();
+   const client = {
+     orders: [100, 401],
+     type: 'INTERNAL'
+   };
 
+   const expected = 0.25;
+
+   // act
+   const actual = service.calculateDiscount(client);
+
+   // assert
+   if (actual === expected) {
+       return;
+     } else {
+       throw new Error(
+         `Assertion failed! Actual="${actual}", where should be "${expected}"`
+       );
+     }
   });
 });
 
 describe('calculateNewOrderValue', () => {
   test('should return value decreased by discount', () => {
+   // arrange
+   const service = new ClientService();
+   const client = {
+     orders: [100, 500],
+   };
 
+   const expected = 540;
+
+   // act
+   const actual = service.calculateNewOrderValue(client, 600);
+
+   // assert
+   if (actual === expected) {
+       return;
+     } else {
+       throw new Error(
+         `Assertion failed! Actual="${actual}", where should be "${expected}"`
+       );
+     }
   });
 
   test('should return value decreased by discount when cleint is INTERNAL', () => {
+   // arrange
+   const service = new ClientService();
+   const client = {
+     orders: [100, 500],
+     type: 'INTERNAL'
+   };
 
+   const expected = 450;
+
+   // act
+   const actual = service.calculateNewOrderValue(client, 600);
+
+   // assert
+   if (actual === expected) {
+       return;
+     } else {
+       throw new Error(
+         `Assertion failed! Actual="${actual}", where should be "${expected}"`
+       );
+     }
   });
 
-  test('should return value decreased by discount when order amoint is less than 500', () => {
+  test('should return value no decreased by discount when order amount is less than 500', () => {
+   // arrange
+   const service = new ClientService();
+   const client = {
+     orders: [100, 399],
+   };
 
+   const expected = 499;
+
+   // act
+   const actual = service.calculateNewOrderValue(client, 499);
+
+   // assert
+   if (actual === expected) {
+       return;
+     } else {
+       throw new Error(
+         `Assertion failed! Actual="${actual}", where should be "${expected}"`
+       );
+     }
   });
 });
