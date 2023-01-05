1d0c8a4f0066f747400cc3d8dd1cdcaa585a7078
Kamil Richert <krichert@atlassian.com>
2023-01-05 12:34:32 +0100

    Ex1 tests

--- tests/exercises/ex1/math.test.js
+++ tests/exercises/ex1/math.test.js
@@ -10,7 +10,13 @@ describe('add', () => {
     });
 
     test('should return negative if both are negative', () => {
-
+        const actual = add(-1, -2);
+        
+        if (actual === -3) {
+            return;
+        } else {
+            throw new Error('Assertion failed!, Should be -3');
+        }
     });
 
 });
@@ -18,24 +24,58 @@ describe('add', () => {
 
 describe('factorial', () => {
     test('should return 6 when arguemnt is 3', () => {
+        const actual = factorial(3);
 
+        if (actual === 6) {
+            return;
+        } else {
+            throw new Error('Assertion failed!, Should be 6');
+        }
     });
 
-    test('should return 0 when arguemnt is 1', () => {
+    test('should return 1 when arguemnt is 0', () => {
+        const actual = factorial(0);
 
+        if (actual === 1) {
+            return;
+        } else {
+            throw new Error('Assertion failed!, Should be 1');
+        }
     });
 });
 
 describe('multiplyOneHaveBePositiveAndNonZero', () => {
     test('should return positive value when both are positivies', () => {
+        const actual = multiplyOneHaveBePositiveAndNonZero(3,4);
 
+        if (actual === 12) {
+            return;
+        } else {
+            throw new Error('Assertion failed!, Should be 12')
+        }
     });
 
     test('should return error when one value is 0', () => {
-
+        try {
+            multiplyOneHaveBePositiveAndNonZero(3,0);
+        } catch (e) {
+            if (e.message === 'Zero not allowed') {
+                return;
+            } else {
+                throw new Error('Assertion failed!, Should be Zero not allowed')
+            }
+        }
     });
 
     test('should return error when both value are negative', () => {
-
+        try {
+            multiplyOneHaveBePositiveAndNonZero(-3,-1);
+        } catch (e) {
+            if (e.message === 'Both numbers have to be positive.') {
+                return;
+            } else {
+                throw new Error('Assertion failed!, Should be Both numbers have to be positive.')
+            }
+        }
     });
 });
