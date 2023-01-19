62c81fae421b62dd5ad849ddc3a09d894c355792
myflowpl <pedro.blaszczak@gmail.com>
2023-01-19 11:23:33 +0100

    express: init nodemon & ts-node

--- node/express-app/src/main.ts
+++ node/express-app/src/main.ts
@@ -1,16 +1,3 @@
 import { resolve } from 'path';
 
-console.log('HELOO FROM EXPRESS 🥳 in:', resolve('./src/main.ts'))
-
-class User {
-
-  name = 'Piotr';
-
-  setName(name: string) {
-    this.name = name;
-  }
-  
-  say() {
-    return 'hello ' + this.name;
-  }
-}
+console.log('HELOO FROM EXPRESS & nodemon', resolve('./src/main.ts'))
